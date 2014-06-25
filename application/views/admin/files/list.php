c392c9e1566b36ba76ff2305eb3fec38c65faf20
andreyka <andreyka.ru@gmail.com>
2014-06-25 12:14:03 +0400

    Add files upload progress bar example

--- application/views/admin/files/list.php
+++ application/views/admin/files/list.php
@@ -67,6 +67,15 @@
 			</ul>
 		</div>
 		
+
+<div class="col-md-12">
+<p>Загрузка файлов</p>
+<div class="progress progress-striped active">
+  <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+    <span class="sr-only">45% Complete</span>
+  </div>
+</div>
+</div>
 		
 
 <!--div class="col-md-12">
