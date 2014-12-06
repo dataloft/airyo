d6b38c9d859b9ea5ce8758272a5b2d61357b93b0
andreyka <andreyka.ru@gmail.com>
2014-12-06 16:15:01 +0400

    Gallery tpl add

---
+++ application/views/startbootstrap/pages/gallery.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="featurette" id="services">
+        <img class="featurette-image img-circle img-responsive pull-left" src="<?php echo $page['img']; ?>">
+        <h2 class="featurette-heading"><?php echo $page['h1']; ?></h2>
+        <p class="lead"><?php echo $page['content1']; ?></p>
+    </div>
+</div>
