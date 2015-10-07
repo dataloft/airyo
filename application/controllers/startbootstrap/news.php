6881dfb23ca5ed0e4956f7e17e9faad5dce6dfd9
dnevnick <dnevnick@gmail.com>
2015-10-07 17:03:35 +0500

    Changed news pagination
    Added bootstrap pagination

--- application/controllers/startbootstrap/news.php
+++ application/controllers/startbootstrap/news.php
@@ -21,8 +21,19 @@ class News extends Frontend {
 		$pg['per_page'] = 10;
 		$pg["num_links"] = 5;
 		$pg['page_query_string'] = TRUE;
-		
-	    $this->pagination->initialize($pg);
+		$pg['full_tag_open'] = '<div class="row text-center"><div class="col-lg-12"><ul class="pagination">';
+		$pg['full_tag_close'] = '</ul></div></div>';
+		$pg['prev_tag_open'] = '<li>';
+		$pg['prev_tag_close'] = '</li>';
+		$pg['next_tag_open'] = '<li>';
+		$pg['next_tag_close'] = '</li>';
+		$pg['cur_tag_open'] = '<li class="active"><a href="">';
+		$pg['cur_tag_close'] = '</a></li>';
+		$pg['num_tag_open'] = '<li>';
+		$pg['num_tag_close'] = '</li>';
+
+
+		$this->pagination->initialize($pg);
 	    
 	    // Получаем список новостей для текущей страницы
 	    $this->data['news']  = $this->news_model->getList($this->pagination->per_page, $this->input->get('per_page'));
