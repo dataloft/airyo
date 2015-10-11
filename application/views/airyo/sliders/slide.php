16af319c23ea67f6e7df0c0336a37b14ff77503a
Pavel Kadrov <pkadrov@gmail.com>
2015-10-11 13:05:32 +0300

    Fix presentation of one slide static view

--- application/views/airyo/sliders/slide.php
+++ application/views/airyo/sliders/slide.php
@@ -21,12 +21,6 @@ $this->css = '<link rel="stylesheet" href="/themes/airyo/css/custom.css" />';
 						Добавить изображение
 					</a>
 				</li>
-				<li>
-					<a href="#">
-						<span class="glyphicon glyphicon-edit" style="color: #777"></span>
-						Редактирование слайдера
-					</a>
-				</li>
 			</ul>
 		</div>
 	</div>
@@ -34,90 +28,96 @@ $this->css = '<link rel="stylesheet" href="/themes/airyo/css/custom.css" />';
 	<div class="row slider-wrapper">
 		<div class="col-md-4">
 			<div class="one-slide">
-				<h4>Первый слайд</h4>
-				<img src="/themes/airyo/img/a5.jpg" alt="" class="img-responsive img-slide">
+				<a href="/themes/airyo/img/a5.jpg" target="_blank">
+					<img src="/themes/airyo/img/a5.jpg" alt="" class="img-responsive img-slide">
+				</a>
 				<form action="#">
 					<div class="input-group">
 						<input type="text" name="name" id="name" class="form-control one-slide-input" placeholder="Название">
-						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-input" placeholder="Описание"></textarea>
+						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-textarea" placeholder="Описание"></textarea>
 						<input type="text" name="link" id="link" class="form-control one-slide-input" placeholder="Ссылка">
-						
-						<label for="del" id="del-label"class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del"> Удалить</label>
+						<label for="del" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del"> Удалить</label>
+						<label for="show" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="show" id="show"> Показать</label>
 					</div>
 				</form>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="one-slide">
-				<h4>Второй слайд</h4>
-				<img src="/themes/airyo/img/a7.jpg" alt="" class="img-responsive img-slide">
+				<a href="/themes/airyo/img/a7.jpg" target="_blank">
+					<img src="/themes/airyo/img/a7.jpg" alt="" class="img-responsive img-slide">
+				</a>
 				<form action="#">
 					<div class="input-group">
 						<input type="text" name="name" id="name1" class="form-control one-slide-input" placeholder="Название">
-						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-input" placeholder="Описание"></textarea>
+						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-textarea" placeholder="Описание"></textarea>
 						<input type="text" name="link" id="link1" class="form-control one-slide-input" placeholder="Ссылка">
-						
-						<label for="del1" id="del-label1"class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del1"> Удалить</label>
+						<label for="del1" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del1"> Удалить</label>
+						<label for="show1" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="show" id="show1"> Показать</label>
 					</div>
 				</form>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="one-slide">
-				<h4>Третий слайд</h4>
-				<img src="/themes/airyo/img/a19.jpg" alt="" class="img-responsive img-slide">
+				<a href="/themes/airyo/img/a19.jpg" target="_blank">
+					<img src="/themes/airyo/img/a19.jpg" alt="" class="img-responsive img-slide">
+				</a>
 				<form action="#">
 					<div class="input-group">
 						<input type="text" name="name" id="name2" class="form-control one-slide-input" placeholder="Название">
-						<textarea name="description" id="description2" cols="10" rows="3" class="form-control one-slide-input" placeholder="Описание"></textarea>
+						<textarea name="description" id="description2" cols="10" rows="3" class="form-control one-slide-textarea" placeholder="Описание"></textarea>
 						<input type="text" name="link" id="link2" class="form-control one-slide-input" placeholder="Ссылка">
-						
-						<label for="del2" id="del-label2"class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del2"> Удалить</label>
+						<label for="del2" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del2"> Удалить</label>
+						<label for="show2" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="show" id="show2"> Показать</label>
 					</div>
 				</form>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="one-slide">
-				<h4>Четвертый слайд</h4>
-				<img src="/themes/airyo/img/a3.jpg" alt="" class="img-responsive img-slide">
+				<a href="/themes/airyo/img/a3.jpg" target="_blank">
+					<img src="/themes/airyo/img/a3.jpg" alt="" class="img-responsive img-slide">
+				</a>
 				<form action="#">
 					<div class="input-group">
 						<input type="text" name="name" id="name" class="form-control one-slide-input" placeholder="Название">
-						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-input" placeholder="Описание"></textarea>
+						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-textarea" placeholder="Описание"></textarea>
 						<input type="text" name="link" id="link" class="form-control one-slide-input" placeholder="Ссылка">
-						
-						<label for="del" id="del-label"class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del"> Удалить</label>
+						<label for="del3" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del3"> Удалить</label>
+						<label for="show3" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="show" id="show3"> Показать</label>
 					</div>
 				</form>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="one-slide">
-				<h4>Пятый слайд</h4>
-				<img src="/themes/airyo/img/a13.jpg" alt="" class="img-responsive img-slide">
+				<a href="/themes/airyo/img/a13.jpg" target="_blank">
+					<img src="/themes/airyo/img/a13.jpg" alt="" class="img-responsive img-slide">
+				</a>
 				<form action="#">
 					<div class="input-group">
 						<input type="text" name="name" id="name1" class="form-control one-slide-input" placeholder="Название">
-						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-input" placeholder="Описание"></textarea>
+						<textarea name="description" id="description" cols="10" rows="3" class="form-control one-slide-textarea" placeholder="Описание"></textarea>
 						<input type="text" name="link" id="link1" class="form-control one-slide-input" placeholder="Ссылка">
-						
-						<label for="del1" id="del-label1"class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del1"> Удалить</label>
+						<label for="del4" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del4"> Удалить</label>
+						<label for="show4" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="show" id="show4"> Показать</label>
 					</div>
 				</form>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="one-slide">
-				<h4>Шестой слайд</h4>
-				<img src="/themes/airyo/img/a18.jpg" alt="" class="img-responsive img-slide">
+				<a href="/themes/airyo/img/a18.jpg" target="_blank">
+					<img src="/themes/airyo/img/a18.jpg" alt="" class="img-responsive img-slide">
+				</a>
 				<form action="#">
 					<div class="input-group">
 						<input type="text" name="name" id="name2" class="form-control one-slide-input" placeholder="Название">
-						<textarea name="description" id="description2" cols="10" rows="3" class="form-control one-slide-input" placeholder="Описание"></textarea>
+						<textarea name="description" id="description2" cols="10" rows="3" class="form-control one-slide-textarea" placeholder="Описание"></textarea>
 						<input type="text" name="link" id="link2" class="form-control one-slide-input" placeholder="Ссылка">
-						
-						<label for="del2" id="del-label2"class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del2"> Удалить</label>
+						<label for="del5" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="del" id="del5"> Удалить</label>
+						<label for="show5" class="form-control one-slide-input one-slide-label"><input type="checkbox" name="show" id="show5"> Показать</label>
 					</div>
 				</form>
 			</div>
