74323c65ac42de70df9b25c6b90c5e0942b76408
Pavel Kadrov <pkadrov@gmail.com>
2015-10-12 10:04:47 +0300

    Add dumps from MyAdmin

--- db/slide.sql
+++ db/slide.sql
@@ -1,73 +1,86 @@
--- Valentina Studio --
--- MySQL dump --
--- ---------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 12, 2015 at 10:02 AM
+-- Server version: 5.5.42
+-- PHP Version: 5.4.42
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- ---------------------------------------------------------
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `air`
+--
 
--- CREATE DATABASE "air" -----------------------------------
-CREATE DATABASE IF NOT EXISTS `air` CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `air`;
--- ---------------------------------------------------------
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `airyo_slide`
+--
 
--- CREATE TABLE "airyo_slide" ------------------------------
-DROP TABLE IF EXISTS `airyo_slide` CASCADE;
+CREATE TABLE `airyo_slide` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(511) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `sliders_id` int(11) unsigned NOT NULL,
+  `create_date` datetime NOT NULL,
+  `enabled` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='Слайд';
 
-CREATE TABLE `airyo_slide` ( 
-	`id` Int( 11 ) AUTO_INCREMENT NOT NULL, 
-	`title` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
-	`description` VarChar( 511 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
-	`link` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
-	`sliders_id` Int( 11 ) UNSIGNED NOT NULL, 
-	`create_date` DateTime NOT NULL, 
-	`enabled` Int( 11 ) NOT NULL DEFAULT '1',
-	 PRIMARY KEY ( `id` )
- )
-CHARACTER SET = utf8
-COLLATE = utf8_general_ci
-COMMENT 'Слайд'
-ENGINE = InnoDB
-AUTO_INCREMENT = 19;
--- ---------------------------------------------------------
+--
+-- Dumping data for table `airyo_slide`
+--
 
+INSERT INTO `airyo_slide` (`id`, `title`, `description`, `link`, `sliders_id`, `create_date`, `enabled`) VALUES
+(5, 'Slide #1', 'This is a first slide', 'www.ya.ru', 1, '0000-00-00 00:00:00', 1),
+(6, 'Slide #2', 'This is a second slide', 'http://google.com', 1, '0000-00-00 00:00:00', 1),
+(7, 'Slide #3', 'This is a third slide', 'http://apple.com', 1, '0000-00-00 00:00:00', 1),
+(10, 'Slide 1 from slider 2', 'First slide of second slider', 'http://www.codeigniter.com/', 2, '0000-00-00 00:00:00', 1),
+(11, 'Slide 2 from slider 2', 'Second slide of second slider', 'http://laravel.com', 2, '0000-00-00 00:00:00', 1),
+(15, 'Slide 3 from slider 2', 'Third slide of second slider', 'http://php.net', 2, '0000-00-00 00:00:00', 1),
+(16, 'Slide 4 from slider 2', 'Fourth slide of second slider', 'http://stackoverflow.com/questions/4061293/mysql-cant-create-table-errno-150', 2, '0000-00-00 00:00:00', 1),
+(17, 'Slide 5 from slider 2', 'Fifth slide of second slider', 'http://kinopoisk.ru', 2, '0000-00-00 00:00:00', 1),
+(18, 'Slide 6 from slider 2', 'Sixth slide of second slider', 'http://habrahabr.ru/company/jugru/blog/268607/', 2, '0000-00-00 00:00:00', 1);
 
--- Dump data of "airyo_slide" ------------------------------
-/*!40000 ALTER TABLE `airyo_slide` DISABLE KEYS */
+--
+-- Indexes for dumped tables
+--
 
-BEGIN;
+--
+-- Indexes for table `airyo_slide`
+--
+ALTER TABLE `airyo_slide`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_airyo_sliders_1_idx` (`sliders_id`);
 
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '5', 'Slide #1', 'This is a first slide', 'www.ya.ru', '1', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '6', 'Slide #2', 'This is a second slide', 'http://google.com', '1', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '7', 'Slide #3', 'This is a third slide', 'http://apple.com', '1', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '10', 'Slide 1 from slider 2', 'First slide of second slider', 'http://www.codeigniter.com/', '2', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '11', 'Slide 2 from slider 2', 'Second slide of second slider', 'http://laravel.com', '2', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '15', 'Slide 3 from slider 2', 'Third slide of second slider', 'http://php.net', '2', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '16', 'Slide 4 from slider 2', 'Fourth slide of second slider', 'http://stackoverflow.com/questions/4061293/mysql-cant-create-table-errno-150', '2', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '17', 'Slide 5 from slider 2', 'Fifth slide of second slider', 'http://kinopoisk.ru', '2', '0000-00-00 00:00:00', '1' );
-INSERT INTO `airyo_slide`(`id`,`title`,`description`,`link`,`sliders_id`,`create_date`,`enabled`) VALUES ( '18', 'Slide 6 from slider 2', 'Sixth slide of second slider', 'http://habrahabr.ru/company/jugru/blog/268607/', '2', '0000-00-00 00:00:00', '1' );
-COMMIT;
-/*!40000 ALTER TABLE `airyo_slide` ENABLE KEYS */
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
--- ---------------------------------------------------------
+--
+-- AUTO_INCREMENT for table `airyo_slide`
+--
+ALTER TABLE `airyo_slide`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+--
+-- Constraints for dumped tables
+--
 
+--
+-- Constraints for table `airyo_slide`
+--
+ALTER TABLE `airyo_slide`
+  ADD CONSTRAINT `fk_airyo_slider_sliders` FOREIGN KEY (`sliders_id`) REFERENCES `airyo_sliders` (`id`);
 
--- CREATE INDEX "fk_airyo_sliders_1_idx" -------------------
-CREATE INDEX `fk_airyo_sliders_1_idx` USING BTREE ON `airyo_slide`( `sliders_id` );
--- ---------------------------------------------------------
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- ---------------------------------------------------------
-
-
--- db/sliders.sql
+++ db/sliders.sql
@@ -1,57 +1,64 @@
--- Valentina Studio --
--- MySQL dump --
--- ---------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 12, 2015 at 10:01 AM
+-- Server version: 5.5.42
+-- PHP Version: 5.4.42
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- ---------------------------------------------------------
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `air`
+--
 
--- CREATE DATABASE "air" -----------------------------------
-CREATE DATABASE IF NOT EXISTS `air` CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `air`;
--- ---------------------------------------------------------
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `airyo_sliders`
+--
 
--- CREATE TABLE "airyo_sliders" ----------------------------
-DROP TABLE IF EXISTS `airyo_sliders` CASCADE;
+CREATE TABLE `airyo_sliders` (
+  `id` int(11) unsigned NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `create_date` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Слайдеры';
 
-CREATE TABLE `airyo_sliders` ( 
-	`id` Int( 11 ) UNSIGNED AUTO_INCREMENT NOT NULL, 
-	`title` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
-	`create_date` DateTime NOT NULL,
-	 PRIMARY KEY ( `id` )
- )
-CHARACTER SET = utf8
-COLLATE = utf8_general_ci
-COMMENT 'Слайдеры'
-ENGINE = InnoDB
-AUTO_INCREMENT = 3;
--- ---------------------------------------------------------
+--
+-- Dumping data for table `airyo_sliders`
+--
 
+INSERT INTO `airyo_sliders` (`id`, `title`, `create_date`) VALUES
+(1, 'First slide', '0000-00-00 00:00:00'),
+(2, 'Second slide', '0000-00-00 00:00:00');
 
--- Dump data of "airyo_sliders" ----------------------------
-/*!40000 ALTER TABLE `airyo_sliders` DISABLE KEYS */
+--
+-- Indexes for dumped tables
+--
 
-BEGIN;
+--
+-- Indexes for table `airyo_sliders`
+--
+ALTER TABLE `airyo_sliders`
+  ADD PRIMARY KEY (`id`);
 
-INSERT INTO `airyo_sliders`(`id`,`title`,`create_date`) VALUES ( '1', 'First slide', '0000-00-00 00:00:00' );
-INSERT INTO `airyo_sliders`(`id`,`title`,`create_date`) VALUES ( '2', 'Second slide', '0000-00-00 00:00:00' );
-COMMIT;
-/*!40000 ALTER TABLE `airyo_sliders` ENABLE KEYS */
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
--- ---------------------------------------------------------
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+--
+-- AUTO_INCREMENT for table `airyo_sliders`
+--
+ALTER TABLE `airyo_sliders`
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- ---------------------------------------------------------
-
-
