69e5af1d8f8a975fb1875827b44771a924ab49b7
andreyka <andreyka.ru@gmail.com>
2015-10-23 15:18:29 +0300

    Indent pages controller

--- application/controllers/startbootstrap/pages.php
+++ application/controllers/startbootstrap/pages.php
@@ -4,24 +4,24 @@ class Pages extends Frontend {
 
 	public function __construct() {
 		parent::__construct();
-		
+
 		$this->load->model('startbootstrap/pages_model');
 		$this->load->model('startbootstrap/sliders_model');
-        $this->load->helper('url');
-        $this->load->library('Smart_codes');
+		$this->load->helper('url');
+		$this->load->library('Smart_codes');
 	}
 
 	public function index() {
-		
-        $this->data['page'] = $this->pages_model->getByAlias($this->uri->uri_string(), true);
-        
+
+		$this->data['page'] = $this->pages_model->getByAlias($this->uri->uri_string(), true);
+
 		if ($this->data['page'])
 		{
 			$this->data['page']['content'] = $this->smart_codes->Parse($this->data['page']['content']);
-            $this->data['page']['title'] = $this->data['page']['h1'];
+			$this->data['page']['title'] = $this->data['page']['h1'];
 
-            $sliders_list = $this->sliders_model->get_list();
-            if (!empty($sliders_list)){
+			$sliders_list = $this->sliders_model->get_list();
+			if (!empty($sliders_list)){
 				foreach ($sliders_list  as $row) {
 					$this->data["sliders"][] = array (
 						'id' => $row["id"],
@@ -30,8 +30,8 @@ class Pages extends Frontend {
 					);
 				}
 			}
-            
-            $this->load->view('startbootstrap/pages/'.$this->data['page']['template'], $this->data);
+
+			$this->load->view('startbootstrap/pages/'.$this->data['page']['template'], $this->data);
 		}
 		else {
 			show_404();
